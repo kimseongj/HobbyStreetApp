3f7f0e8ec070543a41ad51e95473b5b9a438e670
kimseongj <ksj8277@naver.com>
2022-11-29 20:11:57 +0900

    commit file

---
+++ hobbyStreetApp/hobbyStreetApp/Presentation/Flow/MainCoordinator.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MainCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController?
+    
+    func start() {
+        
+    }
+    
+    
+}
--- hobbyStreetApp/hobbyStreetApp/ViewController.swift
+++ hobbyStreetApp/hobbyStreetApp/ViewController.swift
@@ -11,16 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let test = UILabel()
         view.backgroundColor = .white
-        view.addSubview(test)
-        test.text = "text" // test를 위해서 출력할 라벨
-        test.translatesAutoresizingMaskIntoConstraints = false
-        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
+    let homeTabbarMenu = HomeViewController()
+    let reciepeTabbarMenu = ReciepeViewController()
+    let homeBarTabbarMenu = HomeBarViewController()
+    
 
+    
+    
 }
 
